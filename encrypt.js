f49c50eff18ed05064f8d260e7f82d9410198f77
Thomas <thomasmcguire.student@careerdevs.com>
2018-09-18 23:07:13 +0000

    encrypt-final

--- encrypt.js
+++ encrypt.js
@@ -1,18 +1,20 @@
 function encrypt(firstInitial, lastInitial, message){
-   var alphabet = ["!","a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", 
+   var alphabet = [" ","a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", 
    "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
    var letterkey = [];
    var encmessage = [];
-   for (let i = 1; i < 27; i++){
+   for (let i = 0; i < 27; i++){
       letterkey[alphabet[i]] = i;
       //letterkey["a"]
 }
   for(let j = 0; j < message.length; j++){
+     
      let sum = letterkey[firstInitial] + letterkey[lastInitial];  
-     console.log(sum);
+     let total = letterkey[message[j]]  * sum;
+     console.log(total);
       
   }
   
     
 }
-encrypt("r", "f", "This is not my dog");
+encrypt("r", "f", "this is not my dog");
