a72156d2a99aa2db1f1073fa13cc62a0d9859981
Thomas <thomasmcguire.student@careerdevs.com>
2018-09-25 23:10:15 +0000

    loops

---
+++ loop.js
@@ -0,0 +1,4 @@
+let cohortsFiveName = [ "paul" , "dan" , "bret", "joush", "tom" ,"christan", "joe" , "mick" , "mellisa" , "nashan", "Alejandra"];
+for (var i = 0; i < cohortsFiveName.length; i++){
+    console.log( "hello " + cohortsFiveName[i] );
+}
