17cf3012dbb4525f054d9b2651a3992f90d6b773
Thomas <thomasmcguire.student@careerdevs.com>
2018-09-25 23:47:06 +0000

    sums

---
+++ sum-numbers-in-range.js
@@ -0,0 +1,11 @@
+function sumAll(arr) {
+    arr.sort(function(a, b) { return a - b; });
+for (var i = 0; i < arr.length; i++){
+    
+}
+
+    console.log(arr);
+    return 1;
+}
+
+sumAll([14,4]);
